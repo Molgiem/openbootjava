b2970fc1d87e441d314d387924e4eeeb4417c0c6
Molgiem <maicolggarzonmelo@gmail.com>
2023-03-28 22:56:41 -0500

    Ejercicio completo Tema 3

--- src/introduccion_a_la_programacion/tema3/Main.java
+++ src/introduccion_a_la_programacion/tema3/Main.java
@@ -1,9 +1,41 @@
+package introduccion_a_la_programacion.tema3;
+
 public class Main {
     public static void main(String[] args) {
 
-        char arroba = '@';
+        int suma = sumatoria(8, 15, 230);
 
-        System.out.println(arroba);
+        System.out.println(suma);
+
+        Coche miCoche = new Coche();
+
+        miCoche.incrementarPuertas();
+
+        System.out.println(miCoche.getNumeroPuertas());
 
     }
+
+    public static int sumatoria(int primerTermino, int segundoTermino, int tercerTermino){
+        return primerTermino + segundoTermino + tercerTermino;
+    }
+}
+
+class Coche {
+
+    private int numeroPuertas;
+
+    public Coche(){
+        numeroPuertas = 0;
+    }
+
+    public void incrementarPuertas(){
+        numeroPuertas++;
+    }
+
+    public int getNumeroPuertas(){
+        return numeroPuertas;
+    }
+
+
+
 }
